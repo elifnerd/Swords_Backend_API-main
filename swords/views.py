7e6a3beca768452d5b25391aa8e89aa3c7c55101
elifnerd <elisabethanne81@gmail.com>
2023-06-21 11:25:20 -0400

    created PUT in views, updated GET&PUT to be more concise. imported django shortcuts in views also

--- swords/views.py
+++ swords/views.py
@@ -1,7 +1,8 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from.serializers import SwordSerializer
+from .serializers import SwordSerializer
 from .models import Sword
 
 @api_view(['GET', 'POST'])
@@ -18,13 +19,15 @@ def swords_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)  
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def swords_detail(request, pk):
-    try:
-        sword = Sword.objects.get(pk=pk)
+    sword = get_object_or_404(Sword, pk=pk)
+    if request.method == 'GET':
         serializer = SwordSerializer(sword);
         return Response(serializer.data)
-    except Sword.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    elif request.method == 'PUT':
+        serializer = SwordSerializer(sword, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
     
-    return Response(pk)
