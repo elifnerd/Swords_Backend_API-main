625d5e102a01e99269d982e2f9048672082a03ed
elifnerd <elisabethanne81@gmail.com>
2023-06-20 21:56:53 -0400

    adding to the serializer in views, changing the response

--- swords/views.py
+++ swords/views.py
@@ -1,8 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from.serializers import SwordSerializer
+from .models import Sword
 
 @api_view(['GET'])
 def swords_list(request):
+    swords = Sword.objects.all()
     
+    serializer = SwordSerializer(swords, many=True)
     
-    return Response('testing')
+    return Response(serializer.data)
